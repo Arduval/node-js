497d2e4d38cbaae3eb17211619eb92dced6ad6be
Arduval <piotr.jurusik.1997@gmail.com>
2021-09-23 17:22:05 +0200

    added 1stclass funct: output inserted var, delayed by 15

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-for (var x=15;x>=0;--x)
+//academical, should hook to a working <input> device
+function insertText()
 {
-    console.log(x);
+    return "Hello World";
 }
+
+//counts 15 iterations, then displays the output of feeded function
+function countdown(text)
+{
+    a = text();
+    for (var x=15;x>0;--x)
+    {
+        console.log('Text in', x ,'interations.');
+    }
+    console.log(a);
+};
+
+countdown(insertText);
